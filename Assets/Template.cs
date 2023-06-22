96e9de40dbde3c25257e44dee9a3e3053bcc5a3e
BulletzFoBrunch <brandon.mcsheehan04@outlook.com>
2023-06-22 14:26:10 -0400

    Make Template

---
+++ Assets/Template.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Template : MonoBehaviour {
+    public KMBombInfo Bomb;
+    public KMAudio Audio;
+    static int ModuleIdCounter = 1;
+    int ModuleId;
+
+    private void Awake()
+    {
+        ModuleId = ModuleIdCounter++;
+        
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
